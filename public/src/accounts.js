6ddb820270c88440d3825a3d8289fd280655f181
thori bonner <sadboi.codes@gmail.com>
2022-01-12 15:23:51 -0500

    code revision

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,57 +1,35 @@
+
+
 function findAccountById(accounts, id) {
-  // return a single account object matching id that is provided
   return accounts.find((account) => account.id === id);
 }
 
 function sortAccountsByLastName(accounts) {
-  // return accounts array sorted a-z by last name
-  // set to lower case for accuracy
   return accounts.sort((acctOne, acctTwo) => (acctOne.name.last.toLowerCase() < acctTwo.name.last.toLowerCase() ? -1 : 1));
 }
 
 function getTotalNumberOfBorrows(account, books) {
-  // return a number representing the amount of times provided
-  // account has checked out books
   return books
-      // reduce books array to only array of borrow objects from each book
       .reduce((allBorrows, book) => {
-          if (!allBorrows) { allBorrows = [ ...book.borrows, ] }
-          else {allBorrows = [ ...allBorrows, ...book.borrows, ] };
+          !allBorrows ? allBorrows = [ ...book.borrows, ] : allBorrows = [ ...allBorrows, ...book.borrows, ];
           return allBorrows;
           }, [])
-      // filter the new reduced array for borrow.id macthing account.id
-      // filter by id per borrow
       .filter(({id}) => id === account.id)
-      // return length of filtered array to get the number
       .length;
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
-  // filter books to only those with 0 index of borrows array of each book
-  // that matches provided account id and is not returned
   const borrowedBooks = books.filter( ( book => {
-    if(!book.borrows[0].returned && book.borrows[0].id === account.id) {
-      return book;
-    }
+    if (!book.borrows[0].returned && book.borrows[0].id === account.id) return book;
   }));
-  // loop through each book of our filtered array to recreate the book object
-  // to include the author object
-  
   for(let i = 0; i < borrowedBooks.length; i++) {
-    // destructuring each book object
     const {id, title, authorId, borrows} = borrowedBooks[i];
     borrowedBooks[i] = {
-      id,
-      title,
-      authorId,
-      author: authors.find(author => author.id === borrowedBooks[i].authorId),
-      borrows,
+      id, title, authorId, author: authors.find(author => author.id === borrowedBooks[i].authorId), borrows,
     };
   }
   return borrowedBooks;
 }
-
-
 
 
 module.exports = {
--- public/src/books.js
+++ public/src/books.js
@@ -1,50 +1,30 @@
+
+
 function findAuthorById(authors, id) {
-  // return author object matching provided id
   return authors.find((author) => author.id === id);
 }
 
 function findBookById(books, id) {
-  // return book object matching provided id
   return books.find((book) => book.id === id);
 }
 
 function partitionBooksByBorrowedStatus(books) {
-  // return an array with two arrays
-  // first array being books out on loan
-  // second array being books in stock
-  return [
-    [
-      ...books.filter((book) => !book.borrows[0].returned)
-    ],
-    [
-      ...books.filter((book) => book.borrows[0].returned)
-    ]
-  ]
+  return [ books.filter((book) => !book.borrows[0].returned), books.filter((book) => book.borrows[0].returned) ]
 }
 
 function getBorrowersForBook({borrows}, accounts) {
-  // return array of account objects with accounts matching borrows
   const returnValue = [];
-  // loop through borrows array of provided book
-  // add account information to each borrow object
   for (const borrow of borrows) {
     const {id, picture, age, name, company, email, registered} = accounts.find((account) => borrow.id === account.id);
     const returned = borrow.returned 
     const newObj = {
-      id,
-      returned,
-      picture,
-      age,
-      name,
-      company,
-      email,
-      registered
+      id, returned, picture, age, name, company, email, registered
     }
-    // limit length to ten accounts
     returnValue.length >= 10 ? returnValue : returnValue.push(newObj);
   }
   return returnValue;
 }
+
 
 module.exports = {
   findAuthorById,
